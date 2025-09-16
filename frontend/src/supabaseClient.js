6b78e193c31f2084397e29ceb4e02f502d3a9863
crypticaarya <sarthakghoderao@gmail.com>
2025-09-16 23:36:24 +0530

    setting up

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(https://qvfomezzmfbmlcsxucnl.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2Zm9tZXp6bWZibWxjc3h1Y25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNDQ2NTgsImV4cCI6MjA3MzYyMDY1OH0.bilpsV4c940HwUBV0LHtX5p8tVgHIEiHEzb3FcyXrIE);
